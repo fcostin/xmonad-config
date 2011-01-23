3a0069bb22c982974b2a91c2ae2f6dc439721820
fcostin <reuben.fletchercostin@gmail.com>
2011-01-23 11:06:50 +1100

    initial commit

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,26 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+-- cargo cult programming
+
+main = do
+	-- fire up xmobar
+	xmobarProc <- spawnPipe "/usr/bin/xmobar /home/fcostin/.xmobarrc"
+	xmonad $ defaultConfig {
+			-- required for xmonad to layout around statusbar
+			manageHook = manageDocks <+> manageHook defaultConfig,
+			layoutHook = avoidStruts $ layoutHook defaultConfig,
+			-- define how we talk to xmobar
+			logHook = dynamicLogWithPP $ xmobarPP {
+					ppOutput = hPutStrLn xmobarProc,
+					ppTitle = xmobarColor "green" "" . shorten 50
+			}
+		}
+		`additionalKeys` [
+			-- bind printscreen to exec scrot... which prints screen
+			((0, xK_Print), spawn "scrot")
+		]
